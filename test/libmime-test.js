435a734286553d22c2ddc89b515d449a2a0eb978
Ross Johnson <ross@mazira.com>
2017-07-03 10:40:11 -0400

    Remove unused code for QP and mime word encoding

--- test/libmime-test.js
+++ test/libmime-test.js
@@ -20,72 +20,6 @@ var expect = chai.expect;
 chai.config.includeStack = true;
 
 describe('libmime', () => {
-    describe('#isPlainText', () => {
-        it('should detect plain text', () => {
-            expect(libmime.isPlainText('abc')).to.be.true;
-            expect(libmime.isPlainText('abc\x02')).to.be.false;
-            expect(libmime.isPlainText('abcõ')).to.be.false;
-        });
- 
-        it('should return true', () => {
-            expect(libmime.isPlainText('az09\t\r\n~!?')).to.be.true;
-        });
-
-        it('should return false on low bits', () => {
-            expect(libmime.isPlainText('az09\n\x08!?')).to.be.false;
-        });
-
-        it('should return false on high bits', () => {
-            expect(libmime.isPlainText('az09\nõ!?')).to.be.false;
-        });
-    });
-
-    describe('#hasLongerLines', () => {
-        it('should detect longer lines', () => {
-            expect(libmime.hasLongerLines('abc\ndef', 5)).to.be.false;
-            expect(libmime.hasLongerLines('juf\nabcdef\nghi', 5)).to.be.true;
-        });
-    });
-
-    describe('#encodeWord', () => {
-        it('should encode quoted-printable', () => {
-            const input = 'See on õhin test';
-            const output = '=?UTF-8?Q?See_on_=C3=B5hin_test?=';
-            expect(libmime.encodeWord(input)).to.equal(output);
-        });
-
-        it('should encode an empty string in quoted-printable', () => {
-            const output = '=?UTF-8?Q??=';
-            expect(libmime.encodeWord('')).to.equal(output);
-        });
-
-        it('should encode base64', () => {
-            const input = 'See on õhin test';
-            const output = '=?UTF-8?B?U2VlIG9uIMO1aGluIHRlc3Q=?=';
-            expect(libmime.encodeWord(input, 'B')).to.equal(output);
-        });
-
-        it('should encode an empty string in base64', () => {
-            const output = '=?UTF-8?B??='
-            expect(libmime.encodeWord('', 'B')).to.equal(output);
-        });
-    });
-
-    describe('#encodeWords', () => {
-        it('should encode Ascii range', () => {
-            var input1 = 'метель" вьюга',
-                input2 = 'метель\'вьюга',
-                input3 = 'Verão você vai adorar!',
-                output1 = '=?UTF-8?Q?=D0=BC=D0=B5=D1=82=D0=B5=D0=BB=D1=8C=22_?= =?UTF-8?Q?=D0=B2=D1=8C=D1=8E=D0=B3=D0=B0?=',
-                output2 = '=?UTF-8?Q?=D0=BC=D0=B5=D1=82=D0=B5=D0=BB=D1=8C=27?= =?UTF-8?Q?=D0=B2=D1=8C=D1=8E=D0=B3=D0=B0?=',
-                output3 = '=?UTF-8?Q?Ver=C3=A3o_voc=C3=AA?= vai adorar!';
-
-            expect(libmime.encodeWords(input1, 'Q', 52)).to.equal(output1);
-            expect(libmime.encodeWords(input2, 'Q', 52)).to.equal(output2);
-            expect(libmime.encodeWords(input3, 'Q', 52)).to.equal(output3);
-        });
-    });
-
     describe('#decodeWords', () => {
         it('should decode single words', () => {
             const input1 = 'Hello: =?UTF-8?q?See_on_=C3=B5hin_test?=';
@@ -126,22 +60,18 @@ describe('libmime', () => {
             expect(libmime.decodeWords(output2)).to.equal(input2);
         });
 
-        it('should split QP on maxLength', () => {
-            var inputStr = 'Jõgeva Jõgeva Jõgeva mugeva Jõgeva Jõgeva Jõgeva Jõgeva Jõgeva',
-                outputStr = '=?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?mugeva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva?=',
-                encoded = libmime.encodeWords(inputStr, 'Q', 16);
+        it('should decode split QP', () => {
+            const input = '=?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?J=C3=B5geva_?= =?UTF-8?Q?mugeva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva_J?= =?UTF-8?Q?=C3=B5geva?=';
+            const output = 'Jõgeva Jõgeva Jõgeva mugeva Jõgeva Jõgeva Jõgeva Jõgeva Jõgeva';
 
-            expect(outputStr).to.equal(encoded);
-            expect(inputStr).to.equal(libmime.decodeWords(encoded));
+            expect(libmime.decodeWords(input)).to.equal(output);
         });
 
-        it('should split base64 on maxLength', () => {
-            var inputStr = 'õõõõõ õõõõõ õõõõõ mugeva õõõõõ õõõõõ õõõõõ õõõõõ Jõgeva',
-                outputStr = '=?UTF-8?B?w7XDtcO1w7XDtSA=?= =?UTF-8?B?w7XDtcO1w7XDtSA=?= =?UTF-8?B?w7XDtcO1w7XDtSBt?= =?UTF-8?B?dWdldmEgw7XDtcO1?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSBKw7VnZXZh?=',
-                encoded = libmime.encodeWords(inputStr, 'B', 30);
+        it('should decode split base64', () => {
+            const input = '=?UTF-8?B?w7XDtcO1w7XDtSA=?= =?UTF-8?B?w7XDtcO1w7XDtSA=?= =?UTF-8?B?w7XDtcO1w7XDtSBt?= =?UTF-8?B?dWdldmEgw7XDtcO1?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSDDtcO1w7U=?= =?UTF-8?B?w7XDtSBKw7VnZXZh?=';
+            const output = 'õõõõõ õõõõõ õõõõõ mugeva õõõõõ õõõõõ õõõõõ õõõõõ Jõgeva';
 
-            expect(outputStr).to.equal(encoded);
-            expect(inputStr).to.equal(libmime.decodeWords(encoded));
+            expect(libmime.decodeWords(input)).to.equal(output);
         });
 
         it('should ignore language param', () => {
@@ -176,73 +106,6 @@ describe('libmime', () => {
             const input = '=?utf-8?Q?=D0=B3=D0=BE=D1=81_?==?utf-8?Q??==?utf-8?Q?(=D0=BF=D0=B5=D1=80=D0=B5=\r\n D0=B4=D0=B0=D0=B9_=D0=BA=D0=BE=D0?=\r\n =?utf-8?Q?=BC=D1=83_=D0=BD=D0=B0=D0=B4=D0=BE_=D1=82=D0=BE=D0=B6=D0=B5?=';
             const output = 'гос (передай кому надо тоже';
             expect(libmime.decodeWords(input)).to.equal(output);
-        });
-    });
-
-    describe('#buildHeaderParam', () => {
-        it('should return unmodified', () => {
-            expect([{
-                key: 'title',
-                value: 'this is just a title'
-            }]).to.deep.equal(libmime.buildHeaderParam('title', 'this is just a title', 500));
-        });
-
-        it('should encode and split ascii', () => {
-            expect([{
-                key: 'title*0',
-                value: 'this '
-            }, {
-                key: 'title*1',
-                value: 'is ju'
-            }, {
-                key: 'title*2',
-                value: 'st a '
-            }, {
-                key: 'title*3',
-                value: 'title'
-            }]).to.deep.equal(libmime.buildHeaderParam('title', 'this is just a title', 5));
-        });
-
-        it('should encode double byte unicode characters', () => {
-            expect([{
-                key: 'title*0*',
-                value: 'utf-8\'\'Unicode%20title%20%F0%9F%98%8A'
-            }]).to.deep.equal(libmime.buildHeaderParam('title', 'Unicode title 😊', 50));
-        });
-
-        it('should encode and split unicode', () => {
-            expect([{
-                key: 'title*0*',
-                value: 'utf-8\'\'this%20is%20'
-            }, {
-                key: 'title*1',
-                value: 'just a title '
-            }, {
-                key: 'title*2*',
-                value: '%C3%B5%C3%A4%C3%B6'
-            }, {
-                key: 'title*3*',
-                value: '%C3%BC'
-            }]).to.deep.equal(libmime.buildHeaderParam('title', 'this is just a title õäöü', 20));
-        });
-
-        it('should encode and split filename with dashes', () => {
-            expect([{
-                key: 'filename*0*',
-                value: 'utf-8\'\'%C6%94------%C6%94------%C6%94------%C6%94'
-            }, {
-                key: 'filename*1*',
-                value: '------%C6%94------%C6%94------%C6%94------.pdf'
-            }]).to.deep.equal(libmime.buildHeaderParam('filename', 'Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------.pdf', 50));
-        });
-
-        it('should encode and decode', () => {
-            var input = 'Lorěm ipsum doloř siť amet, háš peřpetua compřéhenšam at, ei nám modó soleát éxpétěndá! Boňorum vocibůs dignisšim pro ad, ea sensibus efficiendi intellegam ius. Ad nam aperiam delicata voluptaria, vix nobis luptatum ea, ců úsú graeco viďiššě ňusqúam. ';
-            var headerLine = 'content-disposition: attachment; ' + libmime.buildHeaderParam('filename', input, 50).map(function (item) {
-                return item.key + '="' + item.value + '"';
-            }).join('; ');
-            var parsedHeader = libmime.parseHeaderValue(headerLine);
-            expect(input).to.equal(libmime.decodeWords(parsedHeader.params.filename));
         });
     });
 
@@ -335,160 +198,7 @@ describe('libmime', () => {
         });
     });
 
-    describe('#_buildHeaderValue', () => {
-        it('should build header value', () => {
-            expect(libmime.buildHeaderValue({
-                value: 'test'
-            })).to.equal('test');
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: 'b'
-                }
-            })).to.equal('test; a=b');
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: ';'
-                }
-            })).to.equal('test; a=";"');
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: ';"'
-                }
-            })).to.equal('test; a=";\\""');
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: 'b',
-                    c: 'd'
-                }
-            })).to.equal('test; a=b; c=d');
-        });
-
-        it('should handle unicode filename', () => {
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: 'b',
-                    filename: '😁😂 *\'%()<>@,;:\\"[]?=😃😄zzz😊õäöü😓.pdf'
-                }
-            })).to.equal('test; a=b; filename*0*=utf-8\'\'%F0%9F%98%81%F0%9F%98%82%20%2A%27%25%28%29; filename*1*=%3C%3E%40%2C%3B%3A%5C%22%5B%5D%3F%3D%F0%9F%98%83; filename*2*=%F0%9F%98%84zzz%F0%9F%98%8A%C3%B5%C3%A4%C3%B6; filename*3*=%C3%BC%F0%9F%98%93.pdf');
-        });
-
-        it('should handle dashed filename', () => {
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    filename: 'Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------Ɣ------.pdf'
-                }
-            })).to.equal('test; filename*0*=utf-8\'\'%C6%94------%C6%94------%C6%94------%C6%94; filename*1*=------%C6%94------%C6%94------%C6%94------.pdf');
-        });
-
-        it('should split emoji filename', () => {
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    a: 'b',
-                    filename: 'Jõge-vaŽJõge-vaŽJõge-vaŽ.pdf'
-                }
-            })).to.equal('test; a=b; filename*0*=utf-8\'\'J%C3%B5ge-va%C5%BDJ%C3%B5ge-va%C5%BDJ; filename*1*=%C3%B5ge-va%C5%BD.pdf');
-        });
-
-        it('should quote filename with spaces', () => {
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    filename: 'document a.pdf'
-                }
-            })).to.equal('test; filename="document a.pdf"');
-        });
-
-        // For exhaustive list of special characters
-        // Refer: https://www.w3.org/Protocols/rfc1341/4_Content-Type.html
-        it('should quote filename with special characters', () => {
-            // The case of browser downloads when we download multiple files with same name.
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    filename: 'receipt(3).pdf'
-                }
-            })).to.equal('test; filename="receipt(3).pdf"');
-
-            // For headers which are comma separated as in case of multiple from members elements.
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    filename: 'jack,jill.pdf'
-                }
-            })).to.equal('test; filename="jack,jill.pdf"');
-
-            // Added support for some more special characters
-            var correctString = [
-                'space="x y"',
-                'small_bracket_open="x(y"',
-                'small_bracket_close="x)y"',
-                'angle_bracket_open="x<y"',
-                'angle_bracket_close="x>y"',
-                'at_the_rate="x@y"',
-                'semicolon="x;y"',
-                'colon="x:y"',
-                'back_slash="x\\\\y"',
-                'single_quote="x\'y"',
-                'double_quotes="x\\"y"',
-                'forward_slash="x/y"',
-                'big_bracket_open="x[y"',
-                'big_bracket_close="x]y"',
-                'question_mark="x?y"',
-                'comma="x,y"',
-                'equals="x=y"',
-                'negative_in_mid=x-y',
-                'negative_in_start="-x"'
-            ].join('; ');
-
-            expect(libmime.buildHeaderValue({
-                value: 'test',
-                params: {
-                    space: 'x y',
-                    small_bracket_open: 'x(y',
-                    small_bracket_close: 'x)y',
-                    angle_bracket_open: 'x<y',
-                    angle_bracket_close: 'x>y',
-                    at_the_rate: 'x@y',
-                    semicolon: 'x;y',
-                    colon: 'x:y',
-                    back_slash: 'x\\y',
-                    single_quote: 'x\'y',
-                    double_quotes: 'x"y',
-                    forward_slash: 'x/y',
-                    big_bracket_open: 'x[y',
-                    big_bracket_close: 'x]y',
-                    question_mark: 'x?y',
-                    comma: 'x,y',
-                    equals: 'x=y',
-                    negative_in_mid: 'x-y',
-                    negative_in_start: '-x'
-                }
-            })).to.equal('test; ' + correctString);
-
-        });
-    });
-
-    describe('#encodeFlowed', () => {
-        it('should wrap flowed text', () => {
-            var str = 'tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere\r\nFrom\r\n Hello\r\n> abc\r\nabc',
-                folded = 'tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere \r\n' +
-                'tere tere tere tere tere\r\n' +
-                ' From\r\n' +
-                '  Hello\r\n' +
-                ' > abc\r\n' +
-                'abc';
-            expect(libmime.encodeFlowed(str)).to.equal(folded);
-        });
-    });
-
-    describe('#decodeFlowed', () => {
+   describe('#decodeFlowed', () => {
         it('should remove soft line breaks', () => {
             var str = 'tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere\nFrom\n Hello\n> abc\nabc',
                 folded = 'tere tere tere tere tere tere tere tere tere tere tere tere tere tere tere \r\n' +
@@ -547,81 +257,6 @@ describe('libmime', () => {
 
                 expect(converted).to.deep.equal(charset.convert(encoded, encoding));
             });
-        });
-    });
-
-    describe('mimetypes', () => {
-        describe('#detectExtension', () => {
-            it('should find exact match', () => {
-                var extension = 'doc',
-                    contentType = 'application/msword';
-
-                expect(libmime.detectExtension(contentType)).to.equal(extension);
-            });
-
-            it('should find best match', () => {
-                var extension = 'jpeg',
-                    contentType = 'image/jpeg';
-
-                expect(libmime.detectExtension(contentType)).to.equal(extension);
-                expect(libmime.detectExtension('text/plain')).to.equal('txt');
-            });
-
-            it('should find default match', () => {
-                var extension = 'bin',
-                    contentType = 'sugri/mugri';
-
-                expect(libmime.detectExtension(contentType)).to.equal(extension);
-
-                contentType = 'application/octet-stream';
-
-                expect(libmime.detectExtension(contentType)).to.equal(extension);
-            });
-        });
-
-        describe('#detectMimeType', () => {
-            it('should find exact match', () => {
-                var extension = 'doc',
-                    contentType = 'application/msword';
-
-                expect(libmime.detectMimeType(extension)).to.equal(contentType);
-            });
-
-            it('should find best match', () => {
-                var extension = 'index.js',
-                    contentType = 'application/javascript';
-
-                expect(libmime.detectMimeType(extension)).to.equal(contentType);
-            });
-        });
-    });
-
-    describe('#foldLines', () => {
-        it('should Fold long header line', () => {
-            var inputStr = 'Subject: Testin command line kirja õkva kakva mõni tõnis kõllas põllas tõllas rõllas jušla kušla tušla musla',
-                outputStr = 'Subject: Testin command line kirja\r\n' +
-                ' =?UTF-8?Q?=C3=B5kva_kakva_m=C3=B5ni_t=C3=B5nis_k?=\r\n' +
-                ' =?UTF-8?Q?=C3=B5llas_p=C3=B5llas_t=C3=B5llas_r?=\r\n' +
-                ' =?UTF-8?Q?=C3=B5llas_ju=C5=A1la_ku=C5=A1la_tu?= =?UTF-8?Q?=C5=A1la?= musla',
-                encodedHeaderLine = libmime.encodeWords(inputStr, 'Q', 52);
-
-            expect(outputStr).to.equal(libmime.foldLines(encodedHeaderLine, 76));
-        });
-
-        it('should Fold flowed text', () => {
-            var inputStr = 'Testin command line kirja õkva kakva mõni tõnis kõllas põllas tõllas rõllas jušla kušla tušla musla Testin command line kirja õkva kakva mõni tõnis kõllas põllas tõllas rõllas jušla kušla tušla musla',
-                outputStr = 'Testin command line kirja õkva kakva mõni tõnis kõllas põllas tõllas rõllas \r\n' +
-                'jušla kušla tušla musla Testin command line kirja õkva kakva mõni tõnis \r\n' +
-                'kõllas põllas tõllas rõllas jušla kušla tušla musla';
-
-            expect(outputStr).to.equal(libmime.foldLines(inputStr, 76, true));
-        });
-
-        it('should fold one long line', () => {
-            var inputStr = 'Subject: =?UTF-8?Q?=CB=86=C2=B8=C3=81=C3=8C=C3=93=C4=B1=C3=8F=CB=87=C3=81=C3=9B^=C2=B8\\=C3=81=C4=B1=CB=86=C3=8C=C3=81=C3=9B=C3=98^\\=CB=9C=C3=9B=CB=9D=E2=84=A2=CB=87=C4=B1=C3=93=C2=B8^\\=CB=9C=EF=AC=81^\\=C2=B7\\=CB=9C=C3=98^=C2=A3=CB=9C#=EF=AC=81^\\=C2=A3=EF=AC=81^\\=C2=A3=EF=AC=81^\\?=',
-                outputStr = 'Subject:\r\n =?UTF-8?Q?=CB=86=C2=B8=C3=81=C3=8C=C3=93=C4=B1=C3=8F=CB=87=C3=81=C3=9B^=C2=B8\\=C3=81=C4=B1=CB=86=C3=8C=C3=81=C3=9B=C3=98^\\=CB=9C=C3=9B=CB=9D=E2=84=A2=CB=87=C4=B1=C3=93=C2=B8^\\=CB=9C=EF=AC=81^\\=C2=B7\\=CB=9C=C3=98^=C2=A3=CB=9C#=EF=AC=81^\\=C2=A3=EF=AC=81^\\=C2=A3=EF=AC=81^\\?=';
-
-            expect(outputStr).to.equal(libmime.foldLines(inputStr, 76));
         });
     });
 });
--- test/libqp-unit.js
+++ test/libqp-unit.js
@@ -34,39 +34,10 @@ describe('libqp', () => {
         ['foo bar\r\nbaz\r\n', 'foo =\r\nbar \r\nbaz\r\n']
     ];
 
-    const wrapFixtures = [
-        [
-            'tere, tere, vana kere, kuidas sul l=C3=A4heb?',
-            'tere, tere, vana =\r\nkere, kuidas sul =\r\nl=C3=A4heb?'
-        ],
-        [
-            '=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4=C3=A4',
-            '=C3=A4=C3=A4=\r\n=C3=A4=C3=A4=\r\n=C3=A4=C3=A4=\r\n=C3=A4=C3=A4=\r\n=C3=A4=C3=A4'
-        ],
-        [
-            '1234567890123456789=C3=A40', '1234567890123456789=\r\n=C3=A40'
-        ],
-        [
-            '123456789012345678  90', '123456789012345678 =\r\n 90'
-        ]
-    ];
-
     const streamFixture = [
         '123456789012345678  90\r\nõäöüõäöüõäöüõäöüõäöüõäöüõäöüõäöü another line === ',
         '12345678=\r\n90123456=\r\n78=20=20=\r\n90\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=C3=B5=\r\n=C3=A4=\r\n=C3=B6=\r\n=C3=BC=\r\n=20anoth=\r\ner=20lin=\r\ne=20=3D=\r\n=3D=3D=20'
     ];
-
-    describe('#encode', () => {
-        it('shoud encode UTF-8 string to QP', () => {
-            encodeFixtures.forEach((test) => {
-                expect(libqp.encode(test[0])).to.equal(test[1]);
-            });
-        });
-
-        it('shoud encode Buffer to QP', () => {
-            expect(libqp.encode(new Buffer([0x00, 0x01, 0x02, 0x20, 0x03]))).to.equal('=00=01=02 =03');
-        });
-    });
 
     describe('#decode', () => {
         it('shoud decode QP', () => {
@@ -76,60 +47,7 @@ describe('libqp', () => {
         });
     });
 
-    describe('#wrap', () => {
-        it('should wrap long QP encoded lines', () => {
-            wrapFixtures.forEach((test) => {
-                expect(libqp.wrap(test[0], 20)).to.equal(test[1]);
-            });
-        });
-
-        it('should wrap line ending with <CR>', () => {
-            expect(libqp.wrap('alfa palfa kalfa ralfa\r', 10)).to.equal('alfa palf=\r\na kalfa =\r\nralfa\r');
-        });
-    });
-
     describe('QP Streams', () => {
-
-        it('should transform incoming bytes to QP', (done) => {
-            const encoder = new libqp.Encoder({
-                lineLength: 9
-            });
-
-            const bytes = new Buffer(streamFixture[0]);
-            let i = 0;
-            let buf = [];
-            let buflen = 0;
-
-            encoder.on('data', (chunk) => {
-                buf.push(chunk);
-                buflen += chunk.length;
-            });
-
-            encoder.on('end', (chunk) => {
-                if (chunk) {
-                    buf.push(chunk);
-                    buflen += chunk.length;
-                }
-                buf = Buffer.concat(buf, buflen);
-
-                expect(buf.toString()).to.equal(streamFixture[1]);
-                done();
-            });
-
-            var sendNextByte = () => {
-                if (i >= bytes.length) {
-                    return encoder.end();
-                }
-
-                var ord = bytes[i++];
-                encoder.write(new Buffer([ord]));
-                setImmediate(sendNextByte);
-            };
-
-            sendNextByte();
-        });
-
-
         it('should transform incoming QP to bytes', (done) => {
             var decoder = new libqp.Decoder();
 
@@ -165,35 +83,6 @@ describe('libqp', () => {
             };
 
             sendNextByte();
-        });
-
-        it('should transform incoming bytes to QP and back', (done) => {
-            var decoder = new libqp.Decoder();
-            var encoder = new libqp.Encoder();
-            var file = fs.createReadStream(__dirname + '/fixtures/alice.txt');
-
-            var fhash = crypto.createHash('md5');
-            var dhash = crypto.createHash('md5');
-
-            file.pipe(encoder).pipe(decoder);
-
-            file.on('data', (chunk) => {
-                fhash.update(chunk);
-            });
-
-            file.on('end', () => {
-                fhash = fhash.digest('hex');
-            });
-
-            decoder.on('data', (chunk) => {
-                dhash.update(chunk);
-            });
-
-            decoder.on('end', () => {
-                dhash = dhash.digest('hex');
-                expect(fhash).to.equal(dhash);
-                done();
-            });
         });
     });
 });
